7eada9481b47f6fb2920a4b9c8bbc1b2a49f0e65
root <hl050708@qq.com>
2025-07-05 02:04:15 +0800

    lab9

--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,10 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11  //change
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define NDOUBLYINDIRECT (NINDIRECT * NINDIRECT) // add
+#define MAXFILE (NDIRECT + NINDIRECT + NDOUBLYINDIRECT) // change
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +36,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+2];   // Data block addresses,change
 };
 
 // Inodes per block.
@@ -57,4 +58,7 @@ struct dirent {
   ushort inum;
   char name[DIRSIZ];
 };
+
+
+
 
